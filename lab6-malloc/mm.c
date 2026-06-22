8e0efc8930637623f4de7f31da017fb975a596e8
FancyXGit <fancy.fantasy688@gmail.com>
2026-06-22 16:07:46 +0800

    lab06 完成隐式空闲链表

--- lab6-malloc/mm.c
+++ lab6-malloc/mm.c
@@ -65,6 +65,8 @@ team_t team = {
  * 哨兵只有头部
  */
 
+int mm_check(void);
+
 #define MIN_BLOCK_SIZE 32
 
 /* 头哨兵头部 */
@@ -131,7 +133,7 @@ static inline size_t getSize(char *bp)
  * 调用前必须确定头部被正常写入
  * 且该块是空闲的
  */
-static inline void WriteTail(char* bp)
+static inline void WriteTail(char *bp)
 {
     size_t head = *((size_t *)bp);
     size_t *tail = (size_t *)(bp + getSize(bp) - SIZE_T_SIZE);
@@ -151,7 +153,7 @@ static inline int isAlloc(char *bp)
  */
 static inline int isPrevAlloc(char *bp)
 {
-    return (int)(*((size_t *)bp) & PREV_ALLOC_MASK);
+    return (int)(*((size_t *)bp) & PREV_ALLOC_MASK) >> 1;
 }
 
 /*
@@ -186,7 +188,7 @@ static inline int isPrevPrologue(char *bp)
  */
 static inline char *getNextBp(char *bp)
 {
-    int next_bp = bp + getSize(bp);
+    char *next_bp = bp + getSize(bp);
     if (next_bp == epilogue_bp)
     {
         return NULL;
@@ -211,6 +213,14 @@ static inline char *getPrevBp(char *bp)
 }
 
 /*
+ * 辅助函数：求最大值
+ */
+static inline size_t max(size_t a, size_t b)
+{
+    return a >= b ? a : b;
+}
+
+/*
  * mm_init - initialize the malloc package.
  */
 int mm_init(void)
@@ -226,25 +236,35 @@ int mm_init(void)
     epilogue_bp = (char *)p + SIZE_T_SIZE;
     WriteHead(prologue_bp, head);
     WriteHead(epilogue_bp, head);
+    setPrevAlloc(epilogue_bp, 1);
+
+#ifdef DEBUG
+    if (mm_check() == -1)
+    {
+        printf("正在执行函数mm_init(void))\n");
+    }
+#endif
+
     return 0;
 }
 
 /*
- * mm_malloc - Allocate a block by incrementing the brk pointer.
+ * mm_malloc
  *     Always allocate a block whose size is a multiple of the alignment.
  */
 void *mm_malloc(size_t size)
 {
+    size_t arg1 = size;
     // 向上取整到8字节
     // 加上必须的头节点空间
-    size_t need_size = ALIGN(size + 8);
+    // 至少32字节大小
+    size_t need_size = max(32, ALIGN(size + 8));
     // 获取头哨兵的下一个块
     char *curr_bp = prologue_bp;
     // 最后一个有效块，为后面准备
     char *final_bp = curr_bp;
-    while (curr_bp != NULL)
+    while ((curr_bp = getNextBp(curr_bp)) != NULL)
     {
-        curr_bp = getNextBp(curr_bp);
         // 跳过分配块
         if (isAlloc(curr_bp))
         {
@@ -286,7 +306,7 @@ void *mm_malloc(size_t size)
             WriteHead(curr_bp, head_1);
             setPrevAlloc(curr_bp, is_prev_alloc);
             // 写后一个块的头尾
-            char* bp_2 = getNextBp(curr_bp);
+            char *bp_2 = getNextBp(curr_bp);
             WriteHead(bp_2, head_2);
             setPrevAlloc(bp_2, 1);
             WriteTail(bp_2);
@@ -317,7 +337,83 @@ void *mm_malloc(size_t size)
     epilogue_bp += need_size;
     WriteHead(epilogue_bp, PackHead(SIZE_T_SIZE, 1));
     setPrevAlloc(epilogue_bp, 1);
+
+#ifdef DEBUG
+    if (mm_check() == -1)
+    {
+        printf("正在执行函数mm_malloc(size_t size = %zd)\n", arg1);
+    }
+#endif
+
     return bp2ptr(curr_bp);
+}
+
+/*
+ * 递归合并后面的所有空闲块
+ * 如果后一个块为被占用，则终止
+ * 会自动设置合并完成后的下一个块的isPrevAlloc
+ * 注意只有发生合并才会设置isPrevAlloc
+ * bp: 当前空闲块头指针
+ */
+static void CombineLatter(char *bp)
+{
+    // 获取当前块和下一个块
+    char *curr_bp = bp;
+    char *next_bp = getNextBp(curr_bp);
+    // 没有下一个块：终止
+    if (next_bp == NULL)
+    {
+        return;
+    }
+    // 遇到下一个块被占用：终止
+    if (isAlloc(next_bp))
+    {
+        return;
+    }
+    // 保存前一个块是否占用的信息
+    int is_prev_alloc = isPrevAlloc(curr_bp);
+    // 执行合并操作
+    size_t sum_size = getSize(curr_bp) + getSize(next_bp);
+    WriteHead(curr_bp, PackHead(sum_size, 0));
+    setPrevAlloc(curr_bp, is_prev_alloc);
+    WriteTail(curr_bp);
+    // 获取合并之后的下一个块，并设置之前未分配
+    next_bp = getNextBp(curr_bp);
+    // 没有下一个块：终止
+    if (next_bp == NULL)
+    {
+        setPrevAlloc(epilogue_bp, 0);
+        return;
+    }
+    setPrevAlloc(next_bp, 0);
+    // 递归
+    CombineLatter(curr_bp);
+}
+
+/*
+ * 递归合并前面所有空闲块
+ * 如果前面的块被占用，则终止
+ */
+static void CombineFormer(char *bp)
+{
+    char *curr_bp = bp;
+    char *prev_bp = getPrevBp(bp);
+    // 到达堆底或者到达被占用块
+    // 将当前块isPrevAlloc设为1
+    // 终止递归
+    if (prev_bp == NULL)
+    {
+        setPrevAlloc(curr_bp, 1);
+        return;
+    }
+    // 合并操作
+    int is_prev_alloc = isPrevAlloc(prev_bp);
+    size_t sum_size = getSize(curr_bp) + getSize(prev_bp);
+    WriteHead(prev_bp, PackHead(sum_size, 0));
+    setPrevAlloc(prev_bp, is_prev_alloc);
+    WriteTail(prev_bp);
+    // 递归
+    CombineFormer(prev_bp);
 }
 
 /*
@@ -325,6 +421,31 @@ void *mm_malloc(size_t size)
  */
 void mm_free(void *ptr)
 {
+    void *arg1 = ptr;
+    // 处理空指针
+    if (ptr == NULL)
+    {
+        return;
+    }
+    // 释放当前块内存空间
+    char *curr_bp = ptr2bp((char *)ptr);
+    // 设置当前与之后两个块的状态
+    setAlloc(curr_bp, 0);
+    WriteTail(curr_bp);
+    char *next_bp = getNextBp(curr_bp);
+    if (next_bp != NULL)
+        setPrevAlloc(next_bp, 0);
+    else
+        setPrevAlloc(epilogue_bp, 0);
+    // 递归合并前后相邻空闲块
+    CombineLatter(curr_bp);
+    CombineFormer(curr_bp);
+#ifdef DEBUG
+    if (mm_check() == -1)
+    {
+        printf("正在执行函数mm_free(void *ptr = %p)\n", arg1);
+    }
+#endif
 }
 
 /*
@@ -332,23 +453,153 @@ void mm_free(void *ptr)
  */
 void *mm_realloc(void *ptr, size_t size)
 {
-    void *oldptr = ptr;
-    void *newptr;
-    size_t copySize;
-
-    newptr = mm_malloc(size);
-    if (newptr == NULL)
+    // 空指针
+    if (ptr == NULL)
+    {
+        return mm_malloc(size);
+    }
+    // 大小为0
+    if (size == 0)
+    {
+        mm_free(ptr);
         return NULL;
-    copySize = *(size_t *)((char *)oldptr - SIZE_T_SIZE);
-    if (size < copySize)
-        copySize = size;
-    memcpy(newptr, oldptr, copySize);
-    mm_free(oldptr);
-    return newptr;
+    }
+    char *curr_bp = ptr2bp(ptr);
+    size_t need_size = max(32, ALIGN(size + 8));
+    size_t curr_size = getSize(curr_bp);
+    // 需要大小更小，无需改动
+    if (need_size <= curr_size)
+    {
+        return ptr;
+    }
+    // 需要地址更大，另外扩充空间
+    // 看看后面有没有空闲块可以合并
+    char *next_bp = getNextBp(curr_bp);
+    if (next_bp != NULL && !isAlloc(next_bp))
+    {
+        // 下一个块为空，如果加起来够大直接合并，然后重新调用
+        size_t new_size = getSize(curr_bp) + getSize(next_bp);
+        if (new_size >= need_size)
+        {
+            // 看看大小能不能分裂
+            size_t left_size = new_size - need_size;
+            if (left_size >= MIN_BLOCK_SIZE)
+            {
+                // 大小足够，执行分裂
+                char *next_next_bp = curr_bp + need_size;
+                WriteHead(next_next_bp, PackHead(left_size, 0));
+                setPrevAlloc(next_next_bp, 1);
+                WriteTail(next_next_bp);
+                // 设置头
+                int is_prev_alloc = isPrevAlloc(curr_bp);
+                WriteHead(curr_bp, PackHead(need_size, 1));
+                setPrevAlloc(curr_bp, is_prev_alloc);
+                return ptr;
+            }
+            else
+            {
+                // 整段合并
+                int is_prev_alloc = isPrevAlloc(curr_bp);
+                WriteHead(curr_bp, PackHead(new_size, 1));
+                setPrevAlloc(curr_bp, is_prev_alloc);
+                next_bp = getNextBp(curr_bp);
+                if (next_bp != NULL)
+                {
+                    setPrevAlloc(next_bp, 1);
+                }
+                else
+                {
+                    setPrevAlloc(epilogue_bp, 1);
+                }
+                return ptr;
+            }
+        }
+    }
+    // 没有空闲块，另开一处
+    char *old_ptr = (char *)ptr;
+    char *new_ptr = mm_malloc(size);
+    size_t old_data_size = curr_size - SIZE_T_SIZE;
+    memcpy(new_ptr, old_ptr, old_data_size);
+    mm_free(old_ptr);
+    return new_ptr;
+}
+
+/*
+ * 获取块头的辅助函数
+ */
+static inline size_t getHead(char *bp)
+{
+    return *((size_t *)bp);
+}
+
+/*
+ * 获取块尾的辅助函数
+ * 不关心该块是否空闲
+ */
+static inline size_t getTail(char *bp)
+{
+    return *(size_t *)(bp + getSize(bp) - SIZE_T_SIZE);
+}
+
+/*
+ * 打印前后块的信息
+ */
+static inline void PrintBlockInfo(char *prev_bp, char *curr_bp)
+{
+    printf("前一个块信息：地址: %p, 大小: %d, 分配: %d, 前面是否分配:%d\n", prev_bp, getSize(prev_bp), isAlloc(prev_bp), isPrevAlloc(prev_bp));
+    printf("当前块信息：地址: %p, 大小: %d, 分配: %d, 前面是否分配:%d\n", curr_bp, getSize(curr_bp), isAlloc(curr_bp), isPrevAlloc(curr_bp));
+    printf("当前堆头哨兵地址: %p , 尾哨兵地址 %p\n", prologue_bp, epilogue_bp);
+    printf("仅返回最近的一次错误\n");
 }
 
 int mm_check(void)
 {
-    printf("CHECK MM\n");
+    char *prev_bp = prologue_bp;
+    char *curr_bp = prologue_bp;
+    // 遍历检查全部堆块
+    while ((curr_bp = getNextBp(curr_bp)) != NULL)
+    {
+        // 检查是否对齐
+        if ((size_t)curr_bp % 8 != 0)
+        {
+            printf("\n错误！指针未能按8字节对齐！当前块指针 %p\n", curr_bp);
+            PrintBlockInfo(prev_bp, curr_bp);
+            return -1;
+        }
+        // 检查prev_alloc字段
+        int curr_is_prev_alloc = isPrevAlloc(curr_bp);
+        int prev_alloc = isAlloc(prev_bp);
+        if (prev_alloc != curr_is_prev_alloc)
+        {
+            printf("\n错误！位于 %p 的块的 alloc 为 %d , 但是下一个块 %p 的 prev_alloc 为 %d , 不匹配！\n", prev_bp, prev_alloc, curr_bp, curr_is_prev_alloc);
+            PrintBlockInfo(prev_bp, curr_bp);
+            return -1;
+        }
+        // 检查空闲块头尾块是否一致正确
+        if (!isAlloc(curr_bp))
+        {
+            size_t head = getHead(curr_bp);
+            size_t tail = getTail(curr_bp);
+            if (head != tail)
+            {
+                printf("\n错误！位于%p的空闲块块头和块尾不一致,块头为%#zx,块尾为%#zx,不匹配！\n", curr_bp, head, tail);
+                PrintBlockInfo(prev_bp, curr_bp);
+                return -1;
+            }
+        }
+        // 检查两个空闲块是否相邻
+        // 立即合并不能出现相邻空闲块
+        if (!isAlloc(curr_bp))
+        {
+            if (!isAlloc(prev_bp))
+            {
+                printf("\n错误！相邻的两个空闲块,块1地址 %p, 块2地址 %p \n", prev_bp, curr_bp);
+                PrintBlockInfo(prev_bp, curr_bp);
+                return -1;
+            }
+        }
+        // 一轮检查结束，递增prev_bp
+        prev_bp = curr_bp;
+    }
     return 1;
 }
